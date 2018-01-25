4f43410b6eb825db7e3bcfbb21be55fe0870fbf4
Antonio Pagano <apagano@wawand.co>
2018-01-24 20:56:00 -0500

    adding other bunch of missing method comments

--- form/form.go
+++ form/form.go
@@ -11,6 +11,7 @@ type Form struct {
 	*tags.Tag
 }
 
+//SetAuthenticityToken allows tags to work smoothly with Buffalo, it receives the auth token and creates an input hidden with it.
 func (f *Form) SetAuthenticityToken(s string) {
 	f.Prepend(tags.New("input", tags.Options{
 		"value": s,
@@ -19,11 +20,13 @@ func (f *Form) SetAuthenticityToken(s string) {
 	}))
 }
 
+//Label permits to create a label inside a Form
 func (f Form) Label(value string, opts tags.Options) *tags.Tag {
 	opts["body"] = value
 	return tags.New("label", opts)
 }
 
+//New creates a new form from passed options, it sets defaults for method and also handles other methods as PUT by adding _method hidden input.
 func New(opts tags.Options) *Form {
 	if opts["method"] == nil {
 		opts["method"] = "POST"
--- form/form_for.go
+++ form/form_for.go
@@ -13,6 +13,7 @@ import (
 	"github.com/satori/go.uuid"
 )
 
+//FormFor is a form made for a struct
 type FormFor struct {
 	*Form
 	Model      interface{}
@@ -22,6 +23,7 @@ type FormFor struct {
 	Errors     *validate.Errors
 }
 
+//NewFormFor creates a new Formfor with passed options, it also creates the id of the form from the struct name and adds errors if present.
 func NewFormFor(model interface{}, opts tags.Options) *FormFor {
 	rv := reflect.ValueOf(model)
 	if rv.Kind() == reflect.Ptr {
@@ -66,11 +68,13 @@ func loadErrors(opts tags.Options) *validate.Errors {
 	return errors
 }
 
+//CheckboxTag creates a checkbox for a field on the form Struct
 func (f FormFor) CheckboxTag(field string, opts tags.Options) *tags.Tag {
 	f.buildOptions(field, opts)
 	return f.Form.CheckboxTag(opts)
 }
 
+//InputTag creates an input for a field on the form Struct
 func (f FormFor) InputTag(field string, opts tags.Options) *tags.Tag {
 	f.buildOptions(field, opts)
 	f.addFormatTag(field, opts)
@@ -83,12 +87,14 @@ func (f FormFor) HiddenTag(field string, opts tags.Options) *tags.Tag {
 	return f.Form.HiddenTag(opts)
 }
 
+//FileTag creates a input[type=file] for a field name passed
 func (f FormFor) FileTag(field string, opts tags.Options) *tags.Tag {
 	f.buildOptions(field, opts)
 	f.addFormatTag(field, opts)
 	return f.Form.FileTag(opts)
 }
 
+//DateTimeTag creates a input[type=datetime-local] for a field name passed
 func (f FormFor) DateTimeTag(field string, opts tags.Options) *tags.Tag {
 	f.buildOptions(field, opts)
 	f.addFormatTag(field, opts)
@@ -118,16 +124,19 @@ func (f FormFor) addFormatTag(field string, opts tags.Options) {
 	}
 }
 
+//RadioButton creates a radio button for a struct field
 func (f FormFor) RadioButton(field string, opts tags.Options) *tags.Tag {
 	f.buildOptions(field, opts)
 	return f.Form.RadioButton(opts)
 }
 
+//SelectTag creates a select tag for a specified struct field and loads options from the options opject
 func (f FormFor) SelectTag(field string, opts tags.Options) *SelectTag {
 	f.buildOptions(field, opts)
 	return f.Form.SelectTag(opts)
 }
 
+//TextArea creates text area for the specified struct field
 func (f FormFor) TextArea(field string, opts tags.Options) *tags.Tag {
 	f.buildOptions(field, opts)
 	return f.Form.TextArea(opts)
